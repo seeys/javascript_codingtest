126200cf2c63e0f0059aee696baa686441bb5fc7
seeys <es2008e@naver.com>
2022-06-15 00:06:05 +0900

    update ch07

--- ch07/11.js
+++ ch07/11.js
@@ -1,4 +1,29 @@
-function solution(m, songs) {}
+function solution(m, songs) {
+  let answer = 0;
+  let start = Math.max(...songs);
+  let end = songs.reduce((a, b) => a + b, 0);
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (count(mid, songs) <= m) {
+      answer = mid;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return answer;
+}
+function count(m, songs) {
+  let cnt = 1;
+  let sum = 0;
+  for (let x of songs) {
+    if (sum + x > m) {
+      cnt++;
+      sum = x;
+    } else sum += x;
+  }
+  return cnt;
+}
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(solution(3, arr));
--- ch07/12.js
+++ ch07/12.js
@@ -1,4 +1,30 @@
-function solution(c, arr) {}
+function solution(c, arr) {
+  arr.sort((a, b) => a - b);
+  let start = 1; // 최소거리
+  let end = arr[arr.length - 1];
+  let answer = 0;
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (count(mid, arr) >= mid) {
+      answer = mid;
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return answer;
+}
+function count(m, arr) {
+  let cnt = 1;
+  let end = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i + 1] - end >= m) {
+      cnt++;
+      end = arr[i];
+    }
+  }
+  return cnt;
+}
 
 let arr = [1, 2, 8, 4, 9];
 console.log(solution(3, arr));

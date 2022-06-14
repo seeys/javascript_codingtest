3da11ace72c0805a61e014810bde28aef2b0f4e4
seeys <es2008e@naver.com>
2022-06-14 18:53:27 +0900

    update ch05

--- ch05/04.js
+++ ch05/04.js
@@ -1,8 +1,16 @@
 function solution(m, a) {
   let answer = 0;
   let n = a.length;
-  let i = 0;
+  let lt = 0;
   let sum = 0;
+  for (let rt = 0; rt < n; rt++) {
+    sum += a[rt];
+    if (sum > m) {
+      sum -= a[lt];
+      lt++;
+    }
+    answer += rt - lt + 1;
+  }
 }
 let a = [1, 3, 1, 2, 3];
 console.log(solution(5, a));
